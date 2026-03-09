4a178b17849e38fdfc547f23e65126e70c8a584f
C0braaaa <cobragaming0fo@gmail.com>
2026-03-09 15:51:41 +0700

    ✨ Add connection, greeting, and socket timeout settings to email transporter configuration.

--- src/utils/email.js
+++ src/utils/email.js
@@ -13,6 +13,9 @@ const sendEmail = async (options) => {
     tls: {
       rejectUnauthorized: false,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
 
   const mailOptions = {
